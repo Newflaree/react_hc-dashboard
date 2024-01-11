f488b7f6fd925ae5f49aac50c477c28c4c6edf25
Camilo López <camiloj.lopez79@gmail.com>
2024-01-11 19:48:43 -0300

    [CONFIG]: Defined App arquitecture

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
+// React
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// React Router Dom
+import { BrowserRouter } from 'react-router-dom';
+// Material UI
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { appTheme } from './theme';
+// App
 import App from './App.jsx'
+// Globals Styles
 import './styles.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider theme={ appTheme }>
+        <CssBaseline /> 
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
